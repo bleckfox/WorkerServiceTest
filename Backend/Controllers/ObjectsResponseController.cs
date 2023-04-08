b8d617a5907874c91ae3de98953af4ae2f909f26
master <79881790355@ya.ru>
2023-04-08 22:34:46 +0300

    Add controller and model for generated objects

---
+++ Backend/Controllers/ObjectsResponseController.cs
@@ -0,0 +1,46 @@
+﻿using Backend.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Backend.Controllers;
+
+[ApiController]
+[Route("/objects")]
+public class ObjectsResponseController : ControllerBase
+{
+    private readonly ILogger<ObjectsResponseController> _logger;
+
+    private readonly string _testFilePath =
+        Path.GetFullPath(
+            Path.Combine(
+                Directory.GetCurrentDirectory(), 
+                @"testNotebookFile.txt"
+                )
+            );
+
+    public ObjectsResponseController(ILogger<ObjectsResponseController> logger)
+    {
+        _logger = logger;
+    }
+
+    [HttpGet(Name = "GetObjectsResponse")]
+    [ProducesResponseType(typeof(List<GenerateObjectModel>), StatusCodes.Status200OK)]
+    [ProducesResponseType(StatusCodes.Status400BadRequest)]
+    public async Task<IActionResult> GetObjectsResponse([FromQuery] int minValue, [FromQuery] int maxValue)
+    {
+        return minValue switch
+        {
+            > 0 when maxValue > 0 && maxValue >= minValue => Ok(
+                await Task.Run(async () =>
+                    {
+                        string[] lines = await Task.Run(() => System.IO.File.ReadAllLines(_testFilePath));
+                        return Enumerable.Range(1, Random.Shared.Next(minValue, maxValue))
+                            .Select(data => new GenerateObjectModel
+                            {
+                                Text = $"{DateTimeOffset.UtcNow.ToUnixTimeSeconds()}.{lines[Random.Shared.Next(0, lines.Length - 1)]}"
+                            }).ToArray();
+                    })
+                ),
+            _ => BadRequest("Не удалось получить данные! Проверьте входные параметры. Минимальное и максимальное значения не должны быть меньше или равны 0!")
+        };
+    }
+}
---
+++ Backend/Models/GenerateObjectModel.cs
@@ -0,0 +1,9 @@
+﻿namespace Backend.Models;
+
+/// <summary>
+/// Модель для генерируемого объекта
+/// </summary>
+public class GenerateObjectModel
+{
+    public string Text { get; set; } = string.Empty;
+}
